0058ade6c8e8d0e0babf7060f68b22ba12189e12
Luthfi <mail@luthfihm.com>
2015-09-21 23:30:32 +0700

    Add Multiple Client Support in Server

--- src/com/pat/SimpleIrcServer.java
+++ src/com/pat/SimpleIrcServer.java
@@ -2,6 +2,7 @@ package com.pat;
 
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 
@@ -30,8 +31,7 @@ public class SimpleIrcServer {
     public static void simple(SimpleIrcService.Processor processor) {
         try {
             TServerTransport serverTransport = new TServerSocket(9090);
-            TServer server = new TSimpleServer(new
-                    TServer.Args(serverTransport).processor(processor));
+            TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
             System.out.println("Starting the simple server...");
             server.serve();
         } catch (Exception e) {
